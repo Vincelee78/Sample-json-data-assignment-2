2c6a56982b0cf56e83865261796a7dcc1e2b2761
Vincelee78 <vincelee7778@gmail.com>
2021-10-07 11:45:23 +0000

    added json format data

---
+++ index.js
@@ -0,0 +1,98 @@
+data={
+    "case":{
+      "case-id": "1",
+    "body-systems": [
+      "Cardiovascular",
+      "Nervous"
+    ],
+    "tags-applied": [
+      "Ribs",
+      "ulna"
+    ],
+    "medical-institution": "Changi General Hospital",
+    "gender": "Male",
+    "age": "19",
+    "patient-presentation": {
+      "clinical-history": "High Blood Pressure",
+      "Symptoms": "Vomiting",
+      "Signs": "Distressed"
+    },
+    "findings": {
+      "published-date": "2021-10-07",
+      "revisions": "2",
+      "scientific-references": "Kang E. Pediatric ribs: a spectrum of abnormalities.(2002)",
+      "Case-discussion": "Prominent central vascular markings. Clear costophrenic angles.13 pairs of ribs."
+    },
+    "modality": {
+      "method": [
+        "X-ray",
+        "Ultrasound",
+        "Nuclear Medicine"
+      ]
+    }
+  },
+  "case2":{
+    "case-id": "2",
+    "body-systems": [
+      "Skeletal",
+      "Nervous"
+    ],
+    "tags-applied": [
+      "Metacarpal",
+      "Humerus"
+    ],
+    "medical-institution": "NUH",
+    "gender": "Male",
+    "age": "26",
+    "patient-presentation": {
+      "clinical-history": "High Blood Pressure",
+      "Symptoms": "Vomiting",
+      "Signs": "Distressed"
+    },
+    "findings": {
+      "published-date": "2021-10-07",
+      "revisions": "2",
+      "scientific-references": "Kang E. Pediatric ribs: a spectrum of abnormalities.(2002)",
+      "Case-discussion": "Prominent central vascular markings. Clear costophrenic angles.13 pairs of ribs."
+    },
+    "modality": {
+      "method": [
+        "X-ray",
+        "Ultrasound",
+        "Nuclear Medicine"
+      ]
+    }
+  },
+  "case3":{
+    "case-id": "3",
+    "body-systems": [
+      "Skeletal",
+      "Nervous"
+    ],
+    "tags-applied": [
+      "Metacarpal",
+      "Humerus"
+    ],
+    "medical-institution": "Changi General Hospital",
+    "gender": "Male",
+    "age": "19",
+    "patient-presentation": {
+      "clinical-history": "High Blood Pressure",
+      "Symptoms": "Vomiting",
+      "Signs": "Distressed"
+    },
+    "findings": {
+      "published-date": "2021-10-07",
+      "revisions": "2",
+      "scientific-references": "Kang E. Pediatric ribs: a spectrum of abnormalities.(2002)",
+      "Case-discussion": "Prominent central vascular markings. Clear costophrenic angles.13 pairs of ribs."
+    },
+    "modality": {
+      "method": [
+        "X-ray",
+        "Ultrasound",
+        "Nuclear Medicine"
+      ]
+    }
+  }
+  }
